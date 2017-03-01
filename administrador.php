e79877c6ad986a2e0a3d190c5905ce3367e6afb7
dfbarrios <dfbarrios@gmail.com>
2017-03-01 14:52:56 -0500

    El archivo administrador.php esta completo

--- administrador.php
+++ administrador.php
@@ -22,9 +22,31 @@ require_once("conexion.php");
                   <h5>ACOINPREV</h5>
             </header>        
                <div id='imagecandidates'>                                        
-                     <form name='frmcandidates' method='POST'>
-                        Resultados                           
-                      <input type='button' name='btncerrarsesion' value='Cerrar Sesión' id='btncerrarsesion' id='btncerrarsesion'/>    
+                     <form name='frmcandidates' method='POST'>                     
+                        <?php
+                           require_once('conexion.php');
+                           $qr=mysql_query("select * from candidatos;");
+                           echo "<table>
+                                 <tr>
+                                    <th colspan='7'>Resultado</th>
+                                 </tr>";
+                           echo "<tr>";
+                           for($i=1;$i<=7;$i++){
+                              $imgurl="img/candidates/cdt@0".$i.".jpg";
+                              $ncand="Candidato #".$i;                               
+                                 echo "<td><img src=$imgurl tile='$ncand'></img></td>";                              
+                           }
+                           echo "</tr><tr>";
+                           while($r=mysql_fetch_assoc($qr)){
+                               echo "<td>".
+                                       $r['cdtnombres']." ".$r['cdtapellido']."</br>Total Votos: ".$r['cdtvotos']
+                                    ."</td>";
+                           }
+
+                            echo "</tr></table>";
+                        ?>                           
+                      <input type='button' name='btncerrarsesion' value='Cerrar Sesión' id='btncerrarsesion' 
+                        onclick="this.form.action='csesion.php'; this.form.submit();"/>    
                      </form>
                </div>
       </body>
