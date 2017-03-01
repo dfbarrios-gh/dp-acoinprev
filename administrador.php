c1b05578fd507fbfc764b908f77db90a23537970
dfbarrios <dfbarrios@gmail.com>
2017-03-01 14:25:44 -0500

    Modificado cerrar sesion en administradores.php

--- administrador.php
+++ administrador.php
@@ -20,23 +20,12 @@ require_once("conexion.php");
             <header>
                   <h1>Elecciones personero estudiantil</h1>
                   <h5>ACOINPREV</h5>
-            </header>
-<<<<<<< HEAD
-            <div id='imagecandidates'>                                        
-               <form method='POST'>
-                  <input type='button' name='ssesionv' value='Cerrar Sesion' 
-                    onclick="this.form.action='csesion.php'; this.form.submit();"/>
-               </form>
-            </div>
-=======
-                  <div id='imagecandidates'>                                        
-                        <form name='frmcandidates' method='POST'>
-                           Resultados
-                           
-                          <input type='button' name='btnresultados' value='Resultados'/>
-                          <input type='button' name='btncerrarsesion' value='Cerrar Sesión' id='btncerrarsesion'/>    
-                        </form>
-                  </div>
->>>>>>> dd0b7c3a96f668e802cbd9d55bd5fbd3aadd82e4
+            </header>        
+               <div id='imagecandidates'>                                        
+                     <form name='frmcandidates' method='POST'>
+                        Resultados                           
+                      <input type='button' name='btncerrarsesion' value='Cerrar Sesión' id='btncerrarsesion' id='btncerrarsesion'/>    
+                     </form>
+               </div>
       </body>
 </html>
